29645bba198cd82b0a6f8f8037fcf7aee2a31981
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2024-05-23 15:02:10 +0200

    html css +routes des pages homepage et descente, pbm d'affichage du logo dans le header

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,8 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/homepage.css';
+import './styles/descente.css';
+import './media/logo-sodipharm-377x80.png';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
